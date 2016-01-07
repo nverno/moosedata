b9996f5c2c5f84409acbaf07ec428c1f1c2553e2
Noah Peart <noah.v.peart@gmail.com>
2016-01-07 12:31:23 -0500

    copy files from AFS

--- data-raw/R/copy_data.R
+++ data-raw/R/copy_data.R
@@ -3,7 +3,7 @@
 ## Description: Copy data from AFS to directory
 ## Author: Noah Peart
 ## Created: Wed Jan  6 20:10:56 2016 (-0500)
-## Last-Updated: Wed Jan  6 20:13:13 2016 (-0500)
+## Last-Updated: Thu Jan  7 12:24:21 2016 (-0500)
 ##           By: Noah Peart
 ######################################################################
 source('data-raw/R/check_depends.R')
@@ -11,3 +11,22 @@ pkgs <- c('data.table', 'haven', 'dplyr', 'readxl')
 gitpkgs <- c('nverno'='sync.afs')
 check_depends(pkgs, gitpkgs)
 
+##' Copy files from AFS given file extensions.
+##' @title Copy files from AFS 
+##' @param ext File extensions (character vector)
+##' @param outdir Name of directory to copy files to
+##' @param ... Passed to \code{file.copy}
+##' @return TRUE if all files copied successfully.
+copy_data <- function(ext, outdir='temp', ...) {
+    sync.afs::check_afs()
+    filetype <- afs_path <- NULL
+    filepaths <- sync.afs::data_key[filetype %in% ext, afs_path]
+    dir.create(outdir)
+    res <- file.copy(from = file.path(sync.afs::get_afs(), filepaths),
+                     to = outdir, ...)
+    if (!(any(res))) {
+        warning(sprintf("Failed to copy %s", paste(basename(filepaths), collapse=",")))
+        return( FALSE )
+    }
+    TRUE
+}
